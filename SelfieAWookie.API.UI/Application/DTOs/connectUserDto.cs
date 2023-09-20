7c3e22042dbafaf4525dc675c2ae235a3de67e65
Abdoulaye Pangnon KOULIBALY <abdoulaye-pangnon.koulibaly@talan.com>
2023-09-20 15:43:23 +0200

    JWT fixed login bug

---
+++ SelfieAWookie.API.UI/Application/DTOs/connectUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfieAWookie.API.UI.Application.DTOs
+{
+    public class connectUserDto
+    {
+        #region properties
+        public string Login { get; set; }
+        public string Password { get; set; }
+        #endregion
+    }
+}
